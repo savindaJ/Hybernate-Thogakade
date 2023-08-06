6cc74ed0ac2a3541c631c3e60987ac0225eb23c2
SavindaJayasekara <thantrige32@gmail.com>
2023-08-06 09:38:03 +0530

    add customer entity propertys

--- src/main/java/lk/ijse/thogakade/entity/Customer.java
+++ src/main/java/lk/ijse/thogakade/entity/Customer.java
@@ -1,4 +1,24 @@
 package lk.ijse.thogakade.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+
+@Entity
 public class Customer {
+    @Id
+            @Column(name = "customer_ID")
+    String id;
+    @Column(name = "customer_name")
+    String name;
+    @Column(name = "customer_address")
+    String address;
+    @Column(name = "customer_salary")
+    Double salary;
 }
