0d9aacaacc572e4c88d06be63b95028da337256f
SavindaJayasekara <thantrige32@gmail.com>
2023-08-06 09:40:55 +0530

    add item entity propertist

--- src/main/java/lk/ijse/thogakade/entity/Item.java
+++ src/main/java/lk/ijse/thogakade/entity/Item.java
@@ -1,4 +1,25 @@
 package lk.ijse.thogakade.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+
+@Entity
 public class Item {
+
+    @Id
+            @Column(name = "Item_ID")
+    String id;
+    @Column(name = "Item_description")
+    String description;
+    @Column(name = "Item_unit_price")
+    Double unitPrice;
+    @Column(name = "Item_qtyOnHand")
+    Integer qtyOnHand;
 }
