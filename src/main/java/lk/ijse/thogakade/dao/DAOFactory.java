286304bf43407455f8aa6d2b6c17b2a42f7312c7
SavindaJayasekara <thantrige32@gmail.com>
2023-08-06 17:14:21 +0530

    add dao factory

--- src/main/java/lk/ijse/thogakade/dao/DAOFactory.java
+++ src/main/java/lk/ijse/thogakade/dao/DAOFactory.java
@@ -1,4 +1,22 @@
 package lk.ijse.thogakade.dao;
 
 public class DAOFactory {
+
+    private static DAOFactory factory;
+
+    private DAOFactory(){}
+
+    public static DAOFactory getInstance(){
+        return factory == null ? new DAOFactory() : factory;
+    }
+
+    public enum DAOTypes{
+        CUSTOMERDAO,ITEMDAO,ORDERDAO,ORDERDETAILDAO
+    }
+
+    public <T extends SuperDAO>T getDAO(DAOTypes types){
+        switch (types){
+            case ITEMDAO:
+        }
+    }
 }
