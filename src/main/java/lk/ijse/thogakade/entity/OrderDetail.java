31de372fba0bbf74847595533fa28eaf9f5652f2
SavindaJayasekara <thantrige32@gmail.com>
2023-08-06 09:48:31 +0530

    add order detail propertiest

--- src/main/java/lk/ijse/thogakade/entity/OrderDetail.java
+++ src/main/java/lk/ijse/thogakade/entity/OrderDetail.java
@@ -1,4 +1,21 @@
 package lk.ijse.thogakade.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+
+@Entity
 public class OrderDetail {
+
+    @Id
+    String orderID;
+    @Id
+    String IemID;
+    Integer qty;
+
 }
