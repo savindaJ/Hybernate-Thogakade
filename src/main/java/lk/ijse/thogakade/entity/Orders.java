df898252677a85dd6766cce263ef88571c9ac80f
SavindaJayasekara <thantrige32@gmail.com>
2023-08-06 09:45:49 +0530

    add orders propertiest

--- src/main/java/lk/ijse/thogakade/entity/Orders.java
+++ src/main/java/lk/ijse/thogakade/entity/Orders.java
@@ -1,4 +1,24 @@
 package lk.ijse.thogakade.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+
+@Entity
 public class Orders {
+
+    @Id
+            @Column(name = "order_ID")
+    String id;
+    @Column(name = "Order_date")
+    Date date;
+    @Column(name = "customer_ID")
+    String customerId;
 }
