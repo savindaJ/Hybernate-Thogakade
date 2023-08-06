8b1fb1b60d90b6444ce3e8836c5178adae33c8cc
SavindaJayasekara <thantrige32@gmail.com>
2023-08-06 17:04:38 +0530

    add bo factory

--- src/main/java/lk/ijse/thogakade/bo/BoFactory.java
+++ src/main/java/lk/ijse/thogakade/bo/BoFactory.java
@@ -1,4 +1,31 @@
 package lk.ijse.thogakade.bo;
 
+import lk.ijse.thogakade.bo.custom.impl.CustomerBOImpl;
+import lk.ijse.thogakade.bo.custom.impl.ItemBOImpl;
+import lk.ijse.thogakade.bo.custom.impl.OrderBOImpl;
+
 public class BoFactory {
+    private static BoFactory factory;
+
+    private BoFactory(){}
+
+    public static BoFactory getInstance(){
+        return factory == null ? new BoFactory() : factory;
+    }
+
+    public enum BOTypes{
+        CUSTOMER,ITEM,ORDER
+    }
+    public <T extends SuperBO>T getBo(BOTypes types){
+        switch (types){
+            case ITEM:
+                return (T) new ItemBOImpl();
+            case CUSTOMER:
+                return (T) new CustomerBOImpl();
+            case ORDER:
+                return (T) new OrderBOImpl();
+            default:
+                return null;
+        }
+    }
 }
