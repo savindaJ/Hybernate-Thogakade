98a09d744120d8deb36a3e962fb5f1db28451837
SavindaJayasekara <thantrige32@gmail.com>
2023-08-06 09:12:29 +0530

    add fxml start method

--- src/main/java/lk/ijse/thogakade/AppInitializer.java
+++ src/main/java/lk/ijse/thogakade/AppInitializer.java
@@ -1,7 +1,29 @@
 package lk.ijse.thogakade;
 
-public class AppInitializer {
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class AppInitializer extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = FXMLLoader.load(getClass().getResource(""));
+        primaryStage.setTitle("thogakade");
+        primaryStage.centerOnScreen();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        TranslateTransition tt = new TranslateTransition(Duration.millis(350), scene.getRoot());
+        tt.setFromX(-scene.getWidth());
+        tt.setToX(0);
+        tt.play();
+    }
+
     public static void main(String[] args) {
-        /*launch(args);*/
+        launch(args);
     }
 }
--- src/main/java/lk/ijse/thogakade/Launcher.java
+++ src/main/java/lk/ijse/thogakade/Launcher.java
@@ -1,4 +1,7 @@
 package lk.ijse.thogakade;
 
 public class Launcher {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
 }
