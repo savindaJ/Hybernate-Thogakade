c1b7c8e147f091f8749d1fa2ea2ef6e0532187b1
SavindaJayasekara <thantrige32@gmail.com>
2023-08-07 08:51:06 +0530

    add event in home interface

--- src/main/java/lk/ijse/thogakade/Launcher.java
+++ src/main/java/lk/ijse/thogakade/Launcher.java
@@ -1,10 +1,5 @@
 package lk.ijse.thogakade;
 
-import lk.ijse.thogakade.configaration.StandardConfig;
-import lk.ijse.thogakade.entity.Customer;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
 public class Launcher {
     public static void main(String[] args) {
         AppInitializer.main(args);
--- src/main/java/lk/ijse/thogakade/controller/HomeFormController.java
+++ src/main/java/lk/ijse/thogakade/controller/HomeFormController.java
@@ -1,4 +1,24 @@
 package lk.ijse.thogakade.controller;
 
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class HomeFormController {
+    public Label lblStatus;
+
+    public void mouseEnterde(MouseEvent event) {
+    }
+
+    public void mouseExsit(MouseEvent event) {
+    }
+
+    public void btnCustomerOnAction(MouseEvent event) {
+    }
+
+    public void btnItemOnAction(MouseEvent event) {
+    }
+
+    public void btnOrderOnAction(MouseEvent event) {
+
+    }
 }
