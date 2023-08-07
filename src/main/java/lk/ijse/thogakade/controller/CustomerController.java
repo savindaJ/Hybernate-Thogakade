a699e2863ac73ea3fa678fea57f79c919f704184
SavindaJayasekara <thantrige32@gmail.com>
2023-08-07 09:09:14 +0530

    add customer fotm

--- src/main/java/lk/ijse/thogakade/controller/CustomerController.java
+++ src/main/java/lk/ijse/thogakade/controller/CustomerController.java
@@ -1,4 +1,53 @@
 package lk.ijse.thogakade.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class CustomerController {
+    public AnchorPane root;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtSalary;
+    public JFXTextField txtId;
+    public JFXButton btnSave;
+    public JFXButton btnUpdate;
+    public JFXButton btnDelete;
+    public JFXComboBox cmbId;
+    public JFXButton btnAddNew;
+    public TableView tblCustomer;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cmbIdOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(MouseEvent event) {
+    }
+
+    public void mouseEnterd(MouseEvent event) {
+    }
+
+    public void mouseExit(MouseEvent event) {
+
+    }
 }
