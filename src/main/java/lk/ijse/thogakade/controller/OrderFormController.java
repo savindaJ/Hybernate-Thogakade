72a7035cddd71ee9ed460452dfb44b240828502d
SavindaJayasekara <thantrige32@gmail.com>
2023-08-07 12:11:09 +0530

    add order form propertiest

---
+++ src/main/java/lk/ijse/thogakade/controller/OrderFormController.java
@@ -0,0 +1,29 @@
+package lk.ijse.thogakade.controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class OrderFormController {
+    public AnchorPane root;
+    public Label lblOrderId;
+    public Label lblOrderDate;
+    public JFXComboBox cmbCustomerId;
+    public Label lblCustomerName;
+    public JFXComboBox cmbItemCode;
+    public Label lblDescription;
+    public Label lblUnitPrice;
+    public Label lblQtyOnHand;
+    public TextField txtQty;
+    public TableView tblOrderCart;
+    public TableColumn colItemCode;
+    public TableColumn colDescription;
+    public TableColumn colUnitPrice;
+    public TableColumn colQty;
+    public TableColumn colTotal;
+    public TableColumn colAction;
+    public Label lblNetTotal;
+}
