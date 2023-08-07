dced78aa28f85083fd9b8296064237758e589bd8
SavindaJayasekara <thantrige32@gmail.com>
2023-08-07 12:13:37 +0530

    add order form behaviours

--- src/main/java/lk/ijse/thogakade/controller/OrderFormController.java
+++ src/main/java/lk/ijse/thogakade/controller/OrderFormController.java
@@ -1,13 +1,16 @@
 package lk.ijse.thogakade.controller;
 
 import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class OrderFormController {
+
     public AnchorPane root;
     public Label lblOrderId;
     public Label lblOrderDate;
@@ -26,4 +29,32 @@ public class OrderFormController {
     public TableColumn colTotal;
     public TableColumn colAction;
     public Label lblNetTotal;
+
+    public void cmbCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cmbItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtQtyOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddToCartOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlaceOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(MouseEvent event) {
+    }
+
+    public void mouseEnterd(MouseEvent event) {
+    }
+
+    public void mouseExit(MouseEvent event) {
+
+    }
 }
